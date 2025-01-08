bfd9b29d6697117e5108fa028561e591a5a2ef2e
kody <w.jamesdevera@gmail.com>
2025-01-08 23:57:44 +0800

    added /login to routes

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,9 +1,10 @@
 import { Router, Request, Response } from "express";
-import { registerHandler } from "../controllers/auth.controller";
+import { loginHandler, registerHandler } from "../controllers/auth.controller";
 
 const router = Router();
 
 // Register Endpoint
 router.post("/register", registerHandler);
+router.post("/login", loginHandler);
 
 export default router;
