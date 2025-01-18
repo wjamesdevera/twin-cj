eee6918cac6f2d529767e0c1317dfc8c00666b64
twixchuya <patriciabianca2424@gmail.com>
2025-01-19 01:21:58 +0800

    added navbar design

--- frontend/app/components/navbar.tsx
+++ frontend/app/components/navbar.tsx
@@ -1,7 +1,50 @@
+"use client"; 
+
+import React, { useState } from "react";
 import styles from "./navbar.module.scss";
 
-const navbar = () => {
-  return <div>navbar</div>;
-};
+export default function Navbar() {
+  const [isNavActive, setIsNavActive] = useState(false);
 
-export default navbar;
+  const toggleNav = () => {
+    setIsNavActive(!isNavActive);
+  };
+
+  return (
+    <nav id="navbar" className={styles.navbar}>
+
+      <div className={styles.logo}>
+        {/*<img src="" alt="Twin CJ Logo" />
+        <h1>Twin CJ</h1>*/} 
+      </div>
+
+      {/* Navigation Links */}
+      <div className={`${styles.navLink} ${isNavActive ? styles.active : ""}`}>
+        <ul>
+          <li><a href="/#home" className={styles.active}>Home</a></li>
+          <li><a href="/#amenities">Amenities</a></li>
+          <li><a href="/#activities">Activities</a></li>
+          <li><a href="/#gallery">Gallery</a></li>
+          <li><a href="/#contact">Contact Us</a></li>
+        </ul>
+      </div>
+
+      {/* Reservation Button */}
+      <div className={styles.reservation}>
+        <button className={styles.reserveBtn}>Book Now</button>
+      </div>
+
+      {/* Hamburger Menu for mobile screens */}
+      <div
+        className={`${styles.hamburger} ${
+          isNavActive ? styles.hamburgerActive : ""
+        }`}
+        onClick={toggleNav}
+      >
+        <span className={styles.line}></span>
+        <span className={styles.line}></span>
+        <span className={styles.line}></span>
+      </div>
+    </nav>
+  );
+}
