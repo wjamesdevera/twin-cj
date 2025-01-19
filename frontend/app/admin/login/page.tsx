d447e619732b5aef3fb3163496836bd705611ca5
kody <w.jamesdevera@gmail.com>
2025-01-19 23:00:38 +0800

    refactor: moved login page to proper folder

--- frontend/app/admin/login/page.tsx
+++ frontend/app/admin/login/page.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import Image from 'next/image';
-import styles from '../admin/login.module.scss';
+import React from "react";
+import Image from "next/image";
+import styles from "./login.module.scss";
 
-export default function Login() {
+export default function Page() {
   return (
     <div className={styles.container}>
-      <div className={styles['left-container']}>
-      <Image src='/assets/login-left-bg.png' alt='logo' layout='fill' objectFit='cover' className='left-bg' />
+      <div className={styles["left-container"]}>
+        <Image
+          src="/assets/login-left-bg.png"
+          alt="logo"
+          layout="fill"
+          objectFit="cover"
+          className="left-bg"
+        />
       </div>
-      <div className={styles['right-container']}>
+      <div className={styles["right-container"]}>
         <div className={styles.title}>
           <h1>twin cj</h1>
         </div>
@@ -16,7 +22,9 @@ export default function Login() {
           <h2>RIVERSIDE RESORT</h2>
         </div>
         <div>
-          <p className={styles["text"]}>Welcome! Please log-in with your admin account.</p>
+          <p className={styles["text"]}>
+            Welcome! Please log-in with your admin account.
+          </p>
         </div>
         <div className={styles.form}>
           <form className={styles["login-form"]}>
