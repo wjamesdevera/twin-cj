3668ff8eb2ac1580b86e80b078d3cb75439269c4
kody <w.jamesdevera@gmail.com>
2025-02-03 00:15:36 +0800

    add initial logout button to admin dashboard page

--- frontend/app/admin/(protected)/page.tsx
+++ frontend/app/admin/(protected)/page.tsx
@@ -1,7 +1,12 @@
-"use client";
+import LogoutButton from "../../components/logout-button";
 
 const page = () => {
-  return <h1>Admin</h1>;
+  return (
+    <div>
+      <h1>Admin</h1>
+      <LogoutButton />
+    </div>
+  );
 };
 
 export default page;
