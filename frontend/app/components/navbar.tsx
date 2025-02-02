0a06a67d9a5449cb70246e9d0ccfd125d184d56a
kody <w.jamesdevera@gmail.com>
2025-02-02 20:55:56 +0800

    refactor: navbar styles

--- frontend/app/components/navbar.tsx
+++ frontend/app/components/navbar.tsx
@@ -2,6 +2,8 @@
 
 import React, { useState } from "react";
 import styles from "./navbar.module.scss";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Navbar() {
   const [isNavActive, setIsNavActive] = useState(false);
@@ -11,53 +13,76 @@ export default function Navbar() {
   };
 
   return (
-    <nav id="navbar" className={styles.navbar}>
-      {/* Logo */}
-      <div className={styles.logo}>
-        <img src="/assets/twincj-logo.png" alt="Twin CJ Logo" />
-      </div>
-
-      {/* Hamburger Menu */}
-      <div
-        className={`${styles.hamburger} ${
-          isNavActive ? styles.hamburgerActive : ""
-        }`}
-        onClick={toggleNav}
-      >
-        <span className={styles.line}></span>
-        <span className={styles.line}></span>
-        <span className={styles.line}></span>
-      </div>
-
-      {/* Menubar for Mobile */}
-      <div
-        className={`${styles.menubar} ${
-          isNavActive ? styles.active : ""
-        }`}
-      >
-        <ul>
-          <li><a href="/#home">Home</a></li>
-          <li><a href="/#amenities">Amenities</a></li>
-          <li><a href="/#activities">Activities</a></li>
-          <li><a href="/#gallery">Gallery</a></li>
-          <li><a href="/#contact">Contact Us</a></li>
-        </ul>
-      </div>
-
-      {/* Navigation Links */}
-      <div className={`${styles.navLink} ${isNavActive ? styles.hidden : ""}`}>
-        <ul>
-          <li><a href="/#home" className={styles.active}>Home</a></li>
-          <li><a href="/#amenities">Amenities</a></li>
-          <li><a href="/#activities">Activities</a></li>
-          <li><a href="/#gallery">Gallery</a></li>
-          <li><a href="/#contact">Contact Us</a></li>
-        </ul>
-      </div>
-
-      {/* Reservation Button */}
-      <div className={`${styles.reservation} ${isNavActive ? styles.hidden : ""}`}>
-        <button className={styles.reserveBtn}>Book Now</button>
+    <nav id="navbar" className={`${styles["navbar-container"]}`}>
+      <div className={`${styles["navbar-wrapper"]} container`}>
+        <div className={styles.logo}>
+          <Image src="/assets/twincj-logo.png" alt="Twin CJ Logo" fill={true} />
+        </div>
+        {/* Hamburger Menu */}
+        <div
+          className={`${styles.hamburger} ${
+            isNavActive ? styles.hamburgerActive : ""
+          }`}
+          onClick={toggleNav}
+        >
+          <span className={styles.line}></span>
+          <span className={styles.line}></span>
+          <span className={styles.line}></span>
+        </div>
+        {/* Menubar for Mobile */}
+        <div
+          className={`${styles.menubar} ${isNavActive ? styles.active : ""}`}
+        >
+          <ul>
+            <li>
+              <Link href="/#home">Home</Link>
+            </li>
+            <li>
+              <Link href="/#amenities">Amenities</Link>
+            </li>
+            <li>
+              <Link href="/#activities">Activities</Link>
+            </li>
+            <li>
+              <Link href="/#gallery">Gallery</Link>
+            </li>
+            <li>
+              <Link href="/#contact">Contact Us</Link>
+            </li>
+          </ul>
+        </div>
+        {/* Navigation Links */}
+        <div
+          className={`${styles.navLink} ${isNavActive ? styles.hidden : ""}`}
+        >
+          <ul>
+            <li>
+              <Link href="/#home" className={styles.active}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/#amenities">Amenities</Link>
+            </li>
+            <li>
+              <Link href="/#activities">Activities</Link>
+            </li>
+            <li>
+              <Link href="/#gallery">Gallery</Link>
+            </li>
+            <li>
+              <Link href="/#contact">Contact Us</Link>
+            </li>
+          </ul>
+        </div>
+        {/* Reservation Button */}
+        <div
+          className={`${styles.reservation} ${
+            isNavActive ? styles.hidden : ""
+          }`}
+        >
+          <button className={styles.reserveBtn}>Book Now</button>
+        </div>{" "}
       </div>
     </nav>
   );
