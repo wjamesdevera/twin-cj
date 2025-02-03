51ebf7d0f7ba4992057062cb48e444d29d5fd3bc
kody <w.jamesdevera@gmail.com>
2025-02-03 08:32:27 +0800

    update admin layout

--- frontend/app/admin/(protected)/layout.tsx
+++ frontend/app/admin/(protected)/layout.tsx
@@ -1,9 +1,16 @@
+import AdminNavbar from "@/app/components/adminNavbar";
 import "../../globals.scss";
 import AuthGuard from "@/app/components/AuthGuard";
+import Sidebar from "@/app/components/sidebar";
 export default function LoginLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <AuthGuard>{children}</AuthGuard>;
+  return (
+    <AuthGuard>
+      <Sidebar isOpen={true} />
+      {children}
+    </AuthGuard>
+  );
 }
