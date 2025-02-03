7a971e0dd2cbd41d362c5ecdc06245cfe956be63
kody <w.jamesdevera@gmail.com>
2025-02-04 07:00:10 +0800

    add protected admin page

--- frontend/app/admin/(protected)/layout.tsx
+++ frontend/app/admin/(protected)/layout.tsx
@@ -1,16 +1,9 @@
-import AdminNavbar from "@/app/components/adminNavbar";
 import "../../globals.scss";
 import AuthGuard from "@/app/components/AuthGuard";
-import Sidebar from "@/app/components/sidebar";
 export default function LoginLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <AuthGuard>
-      <Sidebar isOpen={true} />
-      {children}
-    </AuthGuard>
-  );
+  return <AuthGuard>{children}</AuthGuard>;
 }
