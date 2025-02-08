eb8ed3d6a2e21cb9ad6ed8b4ff25dd7ab614e62e
kody <w.jamesdevera@gmail.com>
2025-02-08 23:06:09 +0800

    refactor auth guard component
    fixed bug login not redirecting

--- frontend/app/components/AuthGuard.tsx
+++ frontend/app/components/AuthGuard.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 import useAuth from "../hooks/useAuth";
 import { Loading } from "./loading";
 import { useRouter } from "next/navigation";
@@ -12,14 +12,11 @@ export default function AuthGuard({
   const { user, isLoading } = useAuth();
   const router = useRouter();
 
-  useEffect(() => {
-    if (!isLoading && !user) {
-      router.push("/admin/login");
-    }
-  }, [user, isLoading, router]);
-
   if (isLoading) return <Loading />;
-  if (!user) return null; // Prevent rendering while redirecting
+  if (!user) {
+    router.push("/admin/login");
+    return null;
+  }
 
   return <>{children}</>;
 }
