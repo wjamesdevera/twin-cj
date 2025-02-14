5d2755fee602d145e19cc62a081157a25d03a308
kurtduterte <keduterte@gmail.com>
2025-02-14 19:51:13 +0800

    added CRUD api for day tours

--- backend/src/routes/service.routes.ts
+++ backend/src/routes/service.routes.ts
@@ -1,18 +1,18 @@
-import { Router } from "express";
+import { Router } from 'express';
 import {
   createDayTourHandler,
   getAllDayToursHandler,
   getDayTourByIdHandler,
   deleteDayTourHandler,
   updateDayTourHandler,
-} from "../controllers/service.controller";
+} from '../controllers/service.controller';
 
 const router = Router();
 
-router.post("/day-tour/create", createDayTourHandler);
-router.get("/day-tours", getAllDayToursHandler);
-router.get("/day-tour/:id", getDayTourByIdHandler);
-router.patch("/day-tour/update/:id", updateDayTourHandler);
-router.delete("/day-tour/delete/:id", deleteDayTourHandler);
+router.post('/day-tour', createDayTourHandler);
+router.get('/day-tours', getAllDayToursHandler);
+router.get('/day-tour/:id', getDayTourByIdHandler);
+router.patch('/day-tour/:id', updateDayTourHandler);
+router.delete('/day-tour/:id', deleteDayTourHandler);
 
 export default router;
