068d96f1fb7495787e7ec06aa3062fdc54804c00
twixchuya <patriciabianca.arellano.cics@ust.edu.ph>
2025-02-17 00:07:46 +0800

    added a chevron on the inquiry type and fixed the color when focused on the input fields

--- frontend/app/components/InquiryForm.tsx
+++ frontend/app/components/InquiryForm.tsx
@@ -73,18 +73,36 @@ const InquiryForm: React.FC = () => {
           </div>
           <div className={styles["form-field"]}>
             <label htmlFor="inquiryType">Inquiry Type</label>
-            <select
-              id="inquiryType"
-              name="inquiryType"
-              value={formData.inquiryType}
-              onChange={handleChange}
-            >
-              <option value="Feedback">Feedback</option>
-              <option value="General Inquiry">General Inquiry</option>
-              <option value="Reservations">Reservations</option>
-              <option value="Activities">Activities</option>
-              <option value="Others">Others</option>
-            </select>
+            <div className={styles["select-wrapper"]}>
+              <select
+                id="inquiryType"
+                name="inquiryType"
+                value={formData.inquiryType}
+                onChange={handleChange}
+              >
+                <option value="Feedback">Feedback</option>
+                <option value="General Inquiry">General Inquiry</option>
+                <option value="Reservations">Reservations</option>
+                <option value="Activities">Activities</option>
+                <option value="Others">Others</option>
+              </select>
+              <span className={styles["chevron-down"]}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 33 33"
+                  fill="none"
+                >
+                  <g opacity="0.5">
+                    <path
+                      d="M8.03643 11.6458C8.43916 11.2431 9.0921 11.2431 9.49483 11.6458L15.9844 18.1353L22.4738 11.6458C22.8766 11.2431 23.5296 11.2431 23.9324 11.6458C24.335 12.0485 24.335 12.7015 23.9324 13.1042L16.7136 20.3229C16.3109 20.7257 15.6579 20.7257 15.2552 20.3229L8.03643 13.1042C7.63369 12.7015 7.63369 12.0485 8.03643 11.6458Z"
+                      fill="#333333"
+                    />
+                  </g>
+                </svg>
+              </span>
+            </div>
           </div>
         </div>
         <div className={styles["form-field"]}>
