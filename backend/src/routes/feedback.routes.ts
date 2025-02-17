fa01ed476f4175b32e518598b4ec06ce7636fe1c
kody <w.jamesdevera@gmail.com>
2025-02-17 18:44:44 +0800

    chang feedback routes handler name

--- backend/src/routes/feedback.routes.ts
+++ backend/src/routes/feedback.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { sendFeedback } from "../controllers/feedback.controller";
+import { sendFeedbackHandler } from "../controllers/feedback.controller";
 
 const router = Router();
 
-router.post("", sendFeedback);
+router.post("", sendFeedbackHandler);
 
 export default router;
