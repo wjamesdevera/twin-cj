c2c959aa79228722c62897a4632c59a1c71bcc44
kody <w.jamesdevera@gmail.com>
2025-02-22 20:52:50 +0800

    refactor upload util

--- backend/src/utils/upload.ts
+++ backend/src/utils/upload.ts
@@ -1,6 +1,8 @@
 import { Request } from "express";
 import multer, { FileFilterCallback } from "multer";
 import path from "path";
+import crypto from "crypto";
+
 const fileFilter = (
   request: Request,
   file: Express.Multer.File,
@@ -16,8 +18,21 @@ const fileFilter = (
   }
 };
 
+const UPLOAD_DIR = path.join(__dirname, "../../uploads");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, UPLOAD_DIR);
+  },
+  filename: (req, file, cb) => {
+    const ext = file.originalname.split(".").pop();
+    cb(null, `${crypto.randomUUID()}.${ext}`);
+  },
+});
+
 export const upload = multer({
   dest: path.join(__dirname, "../uploads"),
   limits: { fileSize: 100000000 },
+  storage: storage,
   fileFilter,
 });
