651339278fe0847144c053ed27533726432a22db
kody <w.jamesdevera@gmail.com>
2025-03-09 19:36:26 +0800

    merge development branch to current branch

--- frontend/app/admin/(protected)/layout.tsx
+++ frontend/app/admin/(protected)/layout.tsx
@@ -2,6 +2,7 @@ import AdminSidebar from "@/app/components/admin-sidebar";
 import "../../globals.scss";
 import AuthGuard from "@/app/components/AuthGuard";
 import styles from "./page.module.scss";
+import AdminNavbar from "@/app/components/admin_navbar";
 export default function LoginLayout({
   children,
 }: Readonly<{
@@ -11,7 +12,10 @@ export default function LoginLayout({
     <AuthGuard>
       <main className={styles.main}>
         <AdminSidebar />
-        {children}
+        <section>
+          <AdminNavbar />
+          {children}
+        </section>
       </main>
     </AuthGuard>
   );
