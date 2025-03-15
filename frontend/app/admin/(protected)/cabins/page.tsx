bf53d9d0754e707c6d3acd9806d00a2f9dd9dc00
Artorisity <ralphkylelabaguis@gmail.com>
2025-03-15 15:31:24 +0800

    changed date to datetime

--- frontend/app/admin/(protected)/cabins/page.tsx
+++ frontend/app/admin/(protected)/cabins/page.tsx
@@ -45,6 +45,10 @@ const formatDate = (isoString?: string) => {
     month: "2-digit",
     day: "2-digit",
     year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
   });
 };
 
