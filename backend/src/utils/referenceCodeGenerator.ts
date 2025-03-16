033219c22cdb97a3983600ddf840f1625772780e
kurtduterte <keduterte@gmail.com>
2025-03-16 23:30:07 +0800

    added reference number generator

--- backend/src/utils/referenceCodeGenerator.ts
+++ backend/src/utils/referenceCodeGenerator.ts
@@ -10,8 +10,9 @@ export const generateReferenceCode = async (): Promise<string> => {
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const day = String(date.getDate()).padStart(2, "0");
     const random = Math.floor(1000 + Math.random() * 9000);
+    const randomChar = String.fromCharCode(65 + Math.floor(Math.random() * 26));
 
-    referenceCode = `B${year}/${month}${day}-${random}`;
+    referenceCode = `B${year}${month}${day}-${randomChar}${random}`;
 
     // Check if the reference code already exists in the database
     const existingBooking = await prisma.booking.findUnique({
