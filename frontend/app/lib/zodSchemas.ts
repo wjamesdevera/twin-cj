1a34f4044eca01ce61b25f1e580fcf7c16cc33a3
kody <w.jamesdevera@gmail.com>
2025-03-19 15:45:05 +0800

    update form validations

---
+++ frontend/app/lib/zodSchemas.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const emailSchema = z.string().email("Invalid Email").trim();
+export const nameSchema = z
+  .string()
+  .min(3, "Name must be at least 3 characters")
+  .trim();
+export const phoneNumberSchema = z
+  .string()
+  .regex(
+    /^09\d{9}$/,
+    "Invalid phone number. Please enter an 11-digit number starting with '09' (e.g., 09658108388)."
+  );
