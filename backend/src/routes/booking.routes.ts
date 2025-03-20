54c4ce0f07c4f3f0d8c8a61194f49237e9994b34
Artorisity <ralphkylelabaguis@gmail.com>
2025-03-20 21:37:30 +0800

    created booking status route

--- backend/src/routes/booking.routes.ts
+++ backend/src/routes/booking.routes.ts
@@ -4,6 +4,7 @@ import {
   // checkAvailabilityHandler,
   createBookingHandler,
   getBookingHandler,
+  getBookingStatusHandler,
 } from "../controllers/booking.controller";
 
 const router = Router();
@@ -11,5 +12,6 @@ const router = Router();
 router.get("/", getBookingHandler);
 router.post("/", upload.single("file"), createBookingHandler);
 // router.post("/check-availability", checkAvailabilityHandler);
+router.get("/status/:referenceCode", getBookingStatusHandler);
 
 export default router;
