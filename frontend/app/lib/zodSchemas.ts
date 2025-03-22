1b2f22a738ecae4d12cb25e5bec2d3f448a28a49
kurtduterte <keduterte@gmail.com>
2025-03-23 02:01:36 +0800

    changed validation in zodSchema

--- frontend/app/lib/zodSchemas.ts
+++ frontend/app/lib/zodSchemas.ts
@@ -29,10 +29,7 @@ export const passwordSchema = z
   .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
   .regex(/[a-z]/, "Password must contain at least one lowercase letter")
   .regex(/[0-9]/, "Password must contain at least one number")
-  .regex(
-    /[\W_]/,
-    "Password must contain at least one special character or underscore"
-  );
+  .regex(/[\W_]/, "Password must contain at least one special character");
 
 export const messageSchema = z
   .string()
