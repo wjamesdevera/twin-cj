a17aaf75f384c9f94e8989223127a0a62501fbd1
kody <w.jamesdevera@gmail.com>
2025-03-27 01:34:56 +0800

    add not found page

--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
+import styles from "./notfound.module.scss";
+import twinCjLogo from "@/public/assets/twin-cj-logo.png";
+import Image from "next/image";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className={styles.main}>
+      <div>
+        <Image src={twinCjLogo} alt="Twin CJ Logo" />
+        <Link href="/">Return Home</Link>
+      </div>
     </div>
   );
 }
