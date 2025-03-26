d77c7174726c41f28930cd395961687e3a6bd252
twixchuya <patriciabianca.arellano.cics@ust.edu.ph>
2025-03-26 23:01:18 +0800

    feat: updated lorem ipsum descriptions in why choose us section

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -161,25 +161,22 @@ export default function page() {
           <div className={`${styles["wrapper"]} container`}>
             <SectionHeader title="WHY CHOOSE US?" />
             <p className={styles["header-description"]}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at
-              neque egestas turpis varius pellentesque vitae sed est. Duis
-              cursus nisi vitae enim pellentesque fringilla.
+              At Twin CJ Riverside Glamping Resort, we specialize in creating
+              unforgettable moments. Whether you’re celebrating a special
+              occasion or capturing timeless memories, our stunning venues and
+              dedicated services ensure a seamless and memorable experience.
             </p>
             <div className={`${styles["two-column"]} ${styles["img-right"]}`}>
               <div className={styles["text-area"]}>
                 <h3 className={styles.title}>GAZEBO</h3>
                 <p className={styles.description}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
-                  at urna euismod, sodales ipsum nec, pretium justo. Aenean
-                  volutpat est at nibh imperdiet faucibus. Fusce pellentesque,
-                  elit sed suscipit pretium, sapien dui tincidunt risus, eu
-                  malesuada nunc arcu eget neque. Aenean in velit arcu. Donec
-                  non mattis sem. Vivamus sed volutpat augue, eget ullamcorper
-                  arcu. Morbi fermentum eu lacus nec commodo. Proin vestibulum
-                  rhoncus mi sed interdum. Vivamus et eros varius, dapibus velit
-                  in, pellentesque dolor. Donec sit amet massa elementum,
-                  venenatis orci et, venenatis nulla. Nam efficitur a ante id
-                  viverra.
+                  Host your next unforgettable event in our scenic Gazebo,
+                  perfect for birthday celebrations, wedding receptions, and
+                  team-building activities. Surrounded by lush greenery and
+                  elegant drapery, it offers a charming and versatile space for
+                  any occasion. Whether you're planning an intimate gathering or
+                  a lively festivity, our Gazebo provides the ideal backdrop.
+                  For inquiries, feel free to message us on our Facebook page!
                 </p>
                 <Button variant="outline" className={styles["outline"]}>
                   Book Now
@@ -200,17 +197,17 @@ export default function page() {
               <div className={styles["text-area"]}>
                 <h3 className={styles.title}>PHOTOSHOOT</h3>
                 <p className={styles.description}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
-                  at urna euismod, sodales ipsum nec, pretium justo. Aenean
-                  volutpat est at nibh imperdiet faucibus. Fusce pellentesque,
-                  elit sed suscipit pretium, sapien dui tincidunt risus, eu
-                  malesuada nunc arcu eget neque. Aenean in velit arcu. Donec
-                  non mattis sem. Vivamus sed volutpat augue, eget ullamcorper
-                  arcu. Morbi fermentum eu lacus nec commodo. Proin vestibulum
-                  rhoncus mi sed interdum. Vivamus et eros varius, dapibus velit
-                  in, pellentesque dolor. Donec sit amet massa elementum,
-                  venenatis orci et, venenatis nulla. Nam efficitur a ante id
-                  viverra.
+                  Capture stunning memories with our exclusive photoshoot
+                  packages at Twin CJ Riverside Glamping Resort. Whether you're
+                  aiming for a simple yet elegant shoot or a grand and elaborate
+                  session, we have the perfect setting for you. Enjoy access to
+                  picturesque locations, including our serene Cabana, cozy Mini
+                  Cabin, spacious Maxi Cabin, or the luxurious Venti Cabin. Each
+                  package offers a unique ambiance, from intimate and charming
+                  spaces to expansive and breathtaking backdrops, ensuring you
+                  have the perfect scene for every shot. Let your creativity
+                  shine as you create timeless memories in a truly enchanting
+                  environment!
                 </p>
                 <Button variant="outline" className={styles["outline"]}>
                   Book Now
