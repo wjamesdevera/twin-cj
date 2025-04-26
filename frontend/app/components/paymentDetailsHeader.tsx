97dd26a6470523624f74e5bcca17e1cab39c1db2
kody <w.jamesdevera@gmail.com>
2025-04-26 18:37:07 +0800

    ♻  replace img with Image

--- frontend/app/components/paymentDetailsHeader.tsx
+++ frontend/app/components/paymentDetailsHeader.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import styles from './paymentDetails.module.scss';
+import React from "react";
+import styles from "./paymentDetails.module.scss";
+import Image from "next/image";
 
 export default function paymentDetailsHeader() {
   return (
     <div className={styles.containerHeader}>
-      <img
+      <Image
         src="/assets/payment-details-header.png"
         alt="Payment Details Header"
         className={styles.header}
