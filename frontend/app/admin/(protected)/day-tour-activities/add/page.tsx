c83dc35350be99de0cc146f58a14add939868334
kody <w.jamesdevera@gmail.com>
2025-04-28 21:14:29 +0800

    ♻  remove unused lines

--- frontend/app/admin/(protected)/day-tour-activities/add/page.tsx
+++ frontend/app/admin/(protected)/day-tour-activities/add/page.tsx
@@ -2,11 +2,8 @@
 
 import React from "react";
 import { useRouter } from "next/navigation";
-import useSWRMutation from "swr/mutation";
 import { IoArrowBack } from "react-icons/io5";
 import DayTourForm from "./form";
-import { options } from "@/app/api";
-import { Loading } from "@/app/components/loading";
 import styles from "./page.module.scss";
 import NotificationModal from "@/app/components/notification_modal";
 export default function CreateDayTour() {
@@ -22,33 +19,6 @@ export default function CreateDayTour() {
     type: "success",
   });
 
-  const { trigger, isMutating } = useSWRMutation(
-    `${options.baseURL}/api/services/day-tours/`,
-    async (key, { arg }: { arg: FormData }) => {
-      const response = await fetch(key, {
-        method: "POST",
-        body: arg,
-      });
-
-      if (response.ok) {
-        setNotification({
-          isOpen: true,
-          message: "Day tour created successfully!",
-          type: "success",
-        });
-
-        router.push("/admin/day-tour-activities");
-      } else {
-        setNotification({
-          isOpen: true,
-          message: "Error creating day tour.",
-          type: "error",
-        });
-        throw new Error("Error creating day tour");
-      }
-    }
-  );
-
   return (
     <div className={styles.page_container}>
       <div className={styles.page_header}>
@@ -59,11 +29,7 @@ export default function CreateDayTour() {
       </div>
 
       <div className={styles.form_container}>
-        {isMutating ? (
-          <Loading />
-        ) : (
-          <DayTourForm trigger={trigger} isMutating={isMutating} />
-        )}
+        <DayTourForm />
       </div>
 
       {notification.isOpen && (
